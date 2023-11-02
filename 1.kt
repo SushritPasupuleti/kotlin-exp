01be13e698a9393af61aaf19a5fee0831b03fc40
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-11-02 10:32:22 +0530

    feat: try out iterables

--- 1.kt
+++ 1.kt
@@ -1,31 +1,68 @@
+val fibItems = arrayOf(12, 45, 34, 22)
+
 fun main() {
-	println("Hello, world!")
+    println("Hello, world!")
 
-	val greet = "Hello, world!"
+    val greet = "Hello, world!"
 
-	println("greet: $greet")
+    println("greet: $greet")
 
-	println("greet sanitized: ${sanitize(greet)}")
+    println("greet sanitized: ${sanitize(greet)}")
 
-	var n: Int? = 10;
+    var n: Int? = 10
 
-	val sanitizedN: Int = sanitizeForFib(n);
+    val sanitizedN: Int = sanitizeForFib(n)
 
-	println("fib($n): ${fib(sanitizedN)}")
+    println("fib($n): ${fib(sanitizedN)}")
+
+    println("Printing fibItems")
+    printArrayItems(batchFib(fibItems))
+
+	println("Converting fibItems to map")
+
+	val fibMap = convertToMap(fibItems)
+
+	println("Printing fibMap")
+
+	printMapItems(fibMap)
 }
 
 fun sanitize(s: String): String {
-	return s.replace(Regex("[^a-zA-Z0-9]"), "_")
+    return s.replace(Regex("[^a-zA-Z0-9]"), "_")
 }
 
 fun sanitizeForFib(n: Int?): Int {
-	if (n == null) throw Exception("n is null")
-	if (n < 0) throw Exception("n is negative")
-	if (n > 20) throw Exception("n is too large")
-	return n
+    if (n == null) throw Exception("n is null")
+    if (n < 0) throw Exception("n is negative")
+    if (n > 20) throw Exception("n is too large")
+    return n
 }
 
 fun fib(n: Int): Int {
-	if (n <= 1) return n
-	return fib(n - 1) + fib(n - 2)
+    if (n <= 1) return n
+    return fib(n - 1) + fib(n - 2)
+}
+
+fun batchFib(items: Array<Int>): Array<Int> {
+    return items.map { fib(it) }.toTypedArray()
+}
+
+fun printArrayItems(items: Array<Int>) {
+    for (item: Int in items) {
+        println(item)
+    }
+}
+
+fun convertToMap(items: Array<Int>): Map<Int, Int> {
+	val map = mutableMapOf<Int, Int>()
+	for (item: Int in items) {
+		map[item] = fib(item)
+	}
+	return map
+}
+
+fun printMapItems(map: Map<Int, Int>) {
+	for ((key, value) in map) {
+		println("$key: $value")
+	}
 }
